c29c3a1671b2abc750524f9cbc4c137cf5464c74
Yutian <u6489809@anu.edu.au>
2022-01-15 13:26:37 +0800

    modify to eval blocksworld uniform 2

--- experiments/eval_blocksworld.py
+++ experiments/eval_blocksworld.py
@@ -22,6 +22,6 @@ if __name__ == "__main__":
         args=get_eval_args(
             configurations=[_CONFIGURATION],
             max_search_time=5*60,
-            checkpoint= "../results/blocksworld_sample5_10/model-best.ckpt",
+            checkpoint= "../results/blocksworld_uniform300_10/model-best.ckpt",
         )
     )
