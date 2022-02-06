5a9eee1709b1a7ed95d6dd4fb97c42666a0f40e5
Yutian <u6489809@anu.edu.au>
2022-02-06 14:41:05 +0800

    stop saving training data

--- experiments/n_puzzle.py
+++ experiments/n_puzzle.py
@@ -27,20 +27,21 @@ if __name__ == "__main__":
             for novel in [0, 2]:
                 for lifted in [True, False]:
                     if not ((search_algo=='novelty' and ((all==False ) or novel==0)) or (novel==0 and lifted==True)):                        
-                        mode={'mode':'train', 'all':all, 'search':search_algo, 'distance': 0, 'novel':novel, 'lifted':lifted}
+                        if count >= 6:
+                            mode={'mode':'train', 'auto_bslr': True, 'all':all, 'search':search_algo, 'distance': 0, 'novel':novel, 'lifted':lifted}
                         # print(count)
                         # print('npuzzle'+'_'+'_'.join([str(i)+'_'+str(j) for i, j in mode.items()]))
-                        # count += 1
-                        train_wrapper(
-                            args=get_training_args(
-                                configurations=[_CONFIGURATION],
-                                # 10 minutes
-                                max_training_time=20 * 60,
-                                num_folds=10,
-                                # batch_size=4,
-                                # learning_rate=0.004,
-                            ),
-                            experiment_type='npuzzle'+'_'+'_'.join([str(i)+'_'+str(j) for i, j in mode.items()]),
-                            mode=mode,
-                        )
+                            count += 1
+                            train_wrapper(
+                                args=get_training_args(
+                                    configurations=[_CONFIGURATION],
+                                    # 10 minutes
+                                    max_training_time=20 * 60,
+                                    num_folds=10,
+                                    # batch_size=4,
+                                    # learning_rate=0.004,
+                                ),
+                                experiment_type='npuzzle'+'_'+'_'.join([str(i)+'_'+str(j) for i, j in mode.items()]),
+                                mode=mode,
+                            )
 
